93f85b9abc86787bf2a1e9b894977511a3b3187e
Shinsuke Sugaya <shinsuke@apache.org>
2021-12-13 22:12:00 +0900

    add wildcard

--- src/main/java/org/codelibs/elasticsearch/fess/FessAnalysisPlugin.java
+++ src/main/java/org/codelibs/elasticsearch/fess/FessAnalysisPlugin.java
@@ -114,8 +114,8 @@ public class FessAnalysisPlugin extends Plugin implements AnalysisPlugin, Mapper
     public Collection<SystemIndexDescriptor> getSystemIndexDescriptors(Settings settings) {
         return Collections.unmodifiableList(Arrays.asList(//
                 new SystemIndexDescriptor(".crawler.*", "Contains crawler data"), //
-                new SystemIndexDescriptor(".suggest", "Contains suggest setting data"), //
-                new SystemIndexDescriptor(".suggest_analyzer", "Contains suggest analyzer data"), //
+                new SystemIndexDescriptor(".suggest*", "Contains suggest setting data"), //
+                new SystemIndexDescriptor(".suggest_analyzer*", "Contains suggest analyzer data"), //
                 new SystemIndexDescriptor(".suggest_array.*", "Contains suggest setting data"), //
                 new SystemIndexDescriptor(".suggest_badword.*", "Contains suggest badword data"), //
                 new SystemIndexDescriptor(".suggest_elevate.*", "Contains suggest elevate data"), //
